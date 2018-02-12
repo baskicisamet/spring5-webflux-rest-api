bd0a70801c739ee2082d90317245cbb6d69c0b59
sam <baskicisamet@gmail.com>
2018-02-12 23:21:20 +0300

    init

---
+++ src/main/java/com/sam/spring5webfluxrestapp/Spring5WebfluxRestAppApplication.java
@@ -0,0 +1,12 @@
+package com.sam.spring5webfluxrestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebfluxRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebfluxRestAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sam/spring5webfluxrestapp/Spring5WebfluxRestAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sam.spring5webfluxrestapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5WebfluxRestAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
