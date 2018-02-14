4e9dd650c121d1be68c0effee2f6bb8293552c84
sam <baskicisamet@gmail.com>
2018-02-14 23:48:44 +0300

    creating category controller

---
+++ src/main/java/com/sam/spring5webfluxrestapp/controller/CategoryContoller.java
@@ -0,0 +1,34 @@
+package com.sam.spring5webfluxrestapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sam.spring5webfluxrestapp.domain.Category;
+import com.sam.spring5webfluxrestapp.repository.CategoryRepository;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class CategoryContoller {
+	
+	private final CategoryRepository categoryRepository;
+
+	public CategoryContoller(CategoryRepository categoryRepository) {
+		super();
+		this.categoryRepository = categoryRepository;
+	}
+	
+	@GetMapping("/api/v1/categories")
+	public Flux<Category> list(){
+		return categoryRepository.findAll();
+	}
+	
+	@GetMapping("/api/v1/categories/{id}")
+	public Mono<Category> getById(@PathVariable String id){
+		return categoryRepository.findById(id);
+	}
+	
+
+}
