82b36a094545f322599ed886edbd6b4dcce86241
sam <baskicisamet@gmail.com>
2018-02-15 00:45:19 +0300

    creating new category

--- src/main/java/com/sam/spring5webfluxrestapp/controller/CategoryController.java
+++ src/main/java/com/sam/spring5webfluxrestapp/controller/CategoryController.java
@@ -1,7 +1,12 @@
 package com.sam.spring5webfluxrestapp.controller;
 
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sam.spring5webfluxrestapp.domain.Category;
@@ -21,14 +26,21 @@ public class CategoryController {
 	}
 	
 	@GetMapping("/api/v1/categories")
-	public Flux<Category> list(){
+	Flux<Category> list(){
 		return categoryRepository.findAll();
 	}
 	
 	@GetMapping("/api/v1/categories/{id}")
-	public Mono<Category> getById(@PathVariable String id){
+	Mono<Category> getById(@PathVariable String id){
 		return categoryRepository.findById(id);
 	}
+	
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping("/api/v1/categories")
+	Mono<Void> create(@RequestBody Publisher<Category> categoryStream){//param can be mono or flux
+		return categoryRepository.saveAll(categoryStream).then();
+	}
+	
 	
 
 }
